6b4315aad36e7b11137ebcf0953f2f51671022e7
adrienshen <adrienshen.dev@gmail.com>
2020-05-13 03:13:45 -0400

    Test prettier pre-commit hook

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,11 +1,8 @@
 import "./scss/index.scss";
 
-// import classNames from "classnames";
 import * as React from "react";
 
 import { Form, TextField } from "..";
-// import { FormAddressType, IShippingAddressFormProps } from "./types";
-
 import { CheckoutNextButton } from "../Button";
 import { useLocalStorage } from "@hooks";
 import { withRouter, generatePath } from "react-router";
@@ -18,49 +15,56 @@ const ContactForm: React.FC<any> = ({
   children,
   history,
 }) => {
-  const { storedValue: contactFields, setValue: setContactFields } = useLocalStorage(
-    'contactFields'
-  );
+  const {
+    storedValue: contactFields,
+    setValue: setContactFields,
+  } = useLocalStorage("contactFields");
 
-  return <div className="address-form">
-    <Form<any>
-      errors={errors}
-      onSubmit={(evt, data) => {
-        evt.preventDefault();
-        setContactFields(data);
-        location.href = generatePath(shippingAddressUrl);
-      }}
-      data={contactFields || null}
-    >
-      {children}
-      <fieldset className="form-fieldset">
-        <TextField
-          label="Full Name"
-          type="given-name"
-          name="firstName"
-          autoComplete="given-name"
-          required
-        />
-        <TextField
-          label="Phone"
-          type="phone"
-          name="phone"
-          autoComplete="phone"
-          required
-        />
-        <TextField
-          label="Email"
-          type="email"
-          name="email"
-          autoComplete="email"
-          required
-        />
-      </fieldset>
-      <CheckoutNextButton className="btn-checkout-continue" type="submit" disabled={loading}>
-        {loading ? "Loading" : 'Next'}
-      </CheckoutNextButton>
-    </Form>
-  </div>
+  return (
+    <div className="address-form">
+      <Form<any>
+        errors={errors}
+        onSubmit={(evt, data) => {
+          evt.preventDefault();
+          setContactFields(data);
+          location.href = generatePath(shippingAddressUrl);
+        }}
+        data={contactFields || null}
+      >
+        {children}
+        <fieldset className="form-fieldset">
+          <TextField
+            label="Full Name"
+            type="given-name"
+            name="firstName"
+            autoComplete="given-name"
+            required
+          />
+          <TextField
+            label="Phone"
+            type="phone"
+            name="phone"
+            autoComplete="phone"
+            required
+          />
+          <TextField
+            label="Email"
+            type="email"
+            name="email"
+            autoComplete="email"
+            required
+          />
+        </fieldset>
+        <CheckoutNextButton
+          className="btn-checkout-continue"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Loading" : "Next"}
+        </CheckoutNextButton>
+      </Form>
+    </div>
+  );
 };
 
 export default withRouter(ContactForm);
