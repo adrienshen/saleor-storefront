32cea9222808e7b6dd615e9a721414bdd83b5b68
nikunjsdev <nikunjsdev@gmail.com>
2020-05-14 11:00:50 +0530

    Items included page design

--- src/@next/components/atoms/RichTextContent/RichTextContent.tsx
+++ src/@next/components/atoms/RichTextContent/RichTextContent.tsx
@@ -1,7 +1,9 @@
 import { sanitize } from "dompurify";
 import draftToHtml from "draftjs-to-html";
 import React from "react";
-
+import { convertFromRaw } from "draft-js";
+import { convertToHTML } from "draft-convert";
+import { BLOCK_TYPE, ENTITY_TYPE } from "draftail";
 import { IProps } from "./types";
 
 export const RichTextContent: React.FC<IProps> = ({ descriptionJson }) => (
@@ -9,9 +11,47 @@ export const RichTextContent: React.FC<IProps> = ({ descriptionJson }) => (
     {descriptionJson && (
       <div
         dangerouslySetInnerHTML={{
-          __html: sanitize(draftToHtml(JSON.parse(descriptionJson))),
+          __html: sanitize(
+            convertToHTML(exporterConfig)(
+              convertFromRaw(JSON.parse(descriptionJson))
+            )
+          ),
         }}
       />
     )}
   </>
 );
+
+const exporterConfig = {
+  blockToHTML: block => {
+    if (block.type === BLOCK_TYPE.BLOCKQUOTE) {
+      return <blockquote />;
+    }
+
+    // Discard atomic blocks, as they get converted based on their entity.
+    if (block.type === BLOCK_TYPE.ATOMIC) {
+      return {
+        start: "",
+        end: "",
+      };
+    }
+
+    return null;
+  },
+
+  entityToHTML: (entity, originalText) => {
+    if (entity.type === ENTITY_TYPE.LINK) {
+      return <a href={entity.data.url}>{originalText}</a>;
+    }
+
+    if (entity.type === ENTITY_TYPE.IMAGE) {
+      return <img src={entity.data.src} alt={entity.data.alt} />;
+    }
+
+    if (entity.type === ENTITY_TYPE.HORIZONTAL_RULE) {
+      return <hr />;
+    }
+
+    return originalText;
+  },
+};
--- src/views/CollectionIndex/ItemsIncluded/Page.tsx
+++ src/views/CollectionIndex/ItemsIncluded/Page.tsx
@@ -1,24 +1,82 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import { PageHeader } from "../../../components/Header/PageHeader";
+import { Select } from "../../../@next/components/atoms/Select";
+import { CardBlock } from "./CardBlock";
 
-export const Page = (props) => {
-
+export const Page = props => {
+  const [quantity, setQuantity] = useState(0);
   const handleClick = () => {
-    props.history.goBack()
-  }
+    props.history.goBack();
+  };
+
+  const onChange = () => {
+    return 1;
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(prevState => prevState - 1);
+    }
+  };
+
+  const increaseQuantity = () => {
+    setQuantity(prevState => prevState + 1);
+  };
 
   return (
-    <div className="inner-page-wrapper">
-      <PageHeader back={true} cart={true} search={true} handleClick={handleClick}/>
+    <div className="inner-page-wrapper items-inner-page">
+      <PageHeader
+        back={true}
+        cart={true}
+        search={true}
+        handleClick={handleClick}
+      />
       <div>
         <div className="wrapper-header">Set Details</div>
-        <div className="wrapper-coming-soon">
-          <span className="text">Coming Soon...</span>
+        <div className="wrapper-sub-header">
+          <h5>Filter cabinets By dimensions</h5>
+        </div>
+        <div className="select-value">
+          <div className="select-value--item">
+            <Select value={10} onChange={value => onChange()} />
+          </div>
+          <div className="select-value--item">
+            <Select value={10} onChange={value => onChange()} />
+          </div>
+          <div className="select-value--item">
+            <Select value={10} onChange={value => onChange()} />
+          </div>
+        </div>
+        <div className="addcart-card-wrapper">
+          <CardBlock
+            decreaseQuantity={decreaseQuantity}
+            increaseQuantity={increaseQuantity}
+            quantity={quantity}
+          />
+          <CardBlock
+            decreaseQuantity={decreaseQuantity}
+            increaseQuantity={increaseQuantity}
+            quantity={quantity}
+          />
+          <CardBlock
+            decreaseQuantity={decreaseQuantity}
+            increaseQuantity={increaseQuantity}
+            quantity={quantity}
+          />
+          <CardBlock
+            decreaseQuantity={decreaseQuantity}
+            increaseQuantity={increaseQuantity}
+            quantity={quantity}
+          />
+          <CardBlock
+            decreaseQuantity={decreaseQuantity}
+            increaseQuantity={increaseQuantity}
+            quantity={quantity}
+          />
         </div>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Page
+export default Page;
