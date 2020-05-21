376aeea95a502d7480e449d5fb3429f48a5f5354
nikunjsdev <nikunjsdev@gmail.com>
2020-05-21 09:21:21 +0530

    Change in design

--- src/components/Collection/DetailComponent.tsx
+++ src/components/Collection/DetailComponent.tsx
@@ -3,10 +3,18 @@ import "./scss/index.scss";
 
 export const DetailComponent = ({ name, value }) => {
   return (
-    <div className="d-flex flex-column">
+    <div>
       <div className="d-flex flex-row data-table">
-        <span className="data-box1">{name}:</span>
-        <span className="data-box2">{value}</span>
+        <div className="data-table-info data-left">
+          <span className="data-table-info-box data-table-info-left">
+            {name}:
+          </span>
+        </div>
+        <div className="data-table-info">
+          <span className="data-table-info-box data-table-info-right">
+            {value}
+          </span>
+        </div>
       </div>
     </div>
   );
