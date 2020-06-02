a53b54547243ce7a5f9a8c1cb8f0c39e8362e485
nikunjsdev <nikunjsdev@gmail.com>
2020-06-02 13:36:32 +0530

    Bind dynamic data of specs

--- src/views/CollectionIndex/ViewDetails/View.tsx
+++ src/views/CollectionIndex/ViewDetails/View.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { TypedCollectionSlugQuery } from "../queries";
 import Page from "./Page";
 
 type ViewProps = RouteComponentProps<{ id: string }>;
 
 const View: React.FC<ViewProps> = ({ match, history }) => {
+  const variables = {
+    id: match.params.id,
+  };
+
   return (
-    <div className="home-page">
-      <div>
-        <Page history={history} />
-      </div>
-    </div>
+    <TypedCollectionSlugQuery errorPolicy="all" variables={variables}>
+      {({ data }) => {
+        return (
+          <div className="home-page">
+            <div>
+              <Page history={history} data={data} />
+            </div>
+          </div>
+        );
+      }}
+    </TypedCollectionSlugQuery>
   );
 };
 
