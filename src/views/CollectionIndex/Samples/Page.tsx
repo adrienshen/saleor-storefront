73ffb403637f1fab0fc5650ca8775678af67a8fb
adrienshen <adrienshen.dev@gmail.com>
2020-06-01 23:49:06 -0400

    Fix variant null

--- src/views/CollectionIndex/Samples/Page.tsx
+++ src/views/CollectionIndex/Samples/Page.tsx
@@ -50,44 +50,46 @@ export const Page = ({ data, cart, collectionId, history }: IPage) => {
       <div>
         <div className="wrapper-header">Samples</div>
         <div className="wrapper-img">
-          {sampleExist > 0 ? (
+          {sampleExist > 0 && data.products.edges[0].node.variants.length ? (
             data.products.edges.map(
-              ({ node: { name, id, pricing, thumbnail, variants } }, idx) => (
-                <div className="wrapper-img-main" key={idx}>
-                  <div
-                    className={
-                      samples
-                        ? samples.indexOf(variants[0].id) > -1
-                          ? "wrapper-img-main-inner withBorder"
-                          : "wrapper-img-main-inner noBorder"
-                        : ""
-                    }
-                  >
-                    <div className="wrapper-img-main-inner--header">
-                      <span>{name}</span>
-                    </div>
-                    <div className="wrapper-img-main-inner--img">
-                      <img
-                        src={thumbnail.url}
-                        onClick={() => selectSample(variants[0].id)}
-                        id={id}
-                        key={idx}
-                      />
-                    </div>
-                    <div className="wrapper-img-main-inner--price">
-                      {pricing.priceRange.start.gross.amount !==
-                        pricing.priceRange.start.net.amount && (
-                        <span className="old-price">
-                          <del>${pricing.priceRange.start.gross.amount}</del>
+              ({ node: { name, id, pricing, thumbnail, variants } }, idx) => {
+                return (
+                  <div className="wrapper-img-main" key={idx}>
+                    <div
+                      className={
+                        samples
+                          ? samples.indexOf(variants[0].id) > -1
+                            ? "wrapper-img-main-inner withBorder"
+                            : "wrapper-img-main-inner noBorder"
+                          : ""
+                      }
+                    >
+                      <div className="wrapper-img-main-inner--header">
+                        <span>{name}</span>
+                      </div>
+                      <div className="wrapper-img-main-inner--img">
+                        <img
+                          src={thumbnail.url}
+                          onClick={() => selectSample(variants[0].id)}
+                          id={id}
+                          key={idx}
+                        />
+                      </div>
+                      <div className="wrapper-img-main-inner--price">
+                        {pricing.priceRange.start.gross.amount !==
+                          pricing.priceRange.start.net.amount && (
+                          <span className="old-price">
+                            <del>${pricing.priceRange.start.gross.amount}</del>
+                          </span>
+                        )}
+                        <span className="new-price">
+                          ${pricing.priceRange.start.net.amount}
                         </span>
-                      )}
-                      <span className="new-price">
-                        ${pricing.priceRange.start.net.amount}
-                      </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
+                );
+              }
             )
           ) : (
             <div className="wrapper-coming-soon">
