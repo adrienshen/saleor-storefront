56606a3d79b832a0af6bee7c718ccba6bc43ef54
nikunjsdev <58289663+nikunjsdev@users.noreply.github.com>
2020-06-08 07:09:43 +0530

    Ft/Checkout Summary Component (#17)
    * Added checkout-summary with storybook
    
    * Added design changes

---
+++ src/checkout/components/CheckoutSummary/stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { CheckoutSummary } from ".";
+
+const DEFAULT_PROPS = {
+  fullName: "Mark Watson",
+  email: "markw@email.com",
+  phoneNumber: "+123456789",
+  addressLineOne: "Some Street 13,San Francisco",
+  addressLineTwo: "22132 CA",
+  addressLineThree: "USA",
+  creditCardEnding: "Visa Card ending **00",
+};
+
+storiesOf("@components/checkout/CheckoutSummary", module)
+  .addParameters({ component: CheckoutSummary })
+  .add("default", () => <CheckoutSummary {...DEFAULT_PROPS} />);
