89f759c81951d0cf9436ad51bc68d836676a53b5
nikunjsdev <nikunjsdev@gmail.com>
2020-06-18 09:17:49 +0530

    Added interface to resolve TS bugs - 7

--- src/@next/components/atoms/Message/types.ts
+++ src/@next/components/atoms/Message/types.ts
@@ -1,7 +1,10 @@
+import { DefaultTheme } from "@styles";
+
 export interface IProps {
   title: string;
   status?: "neutral" | "success" | "error";
   onClick: () => void;
   children?: React.ReactNode;
   actionText?: string;
+  theme?: any;
 }
