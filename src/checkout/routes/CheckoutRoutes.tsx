368693dfee6894be23f5a5129c2a124841dd705a
adrienshen <adrienshen.dev@gmail.com>
2020-06-25 22:12:55 -0700

    Comment DispatchRoute

--- src/checkout/routes/CheckoutRoutes.tsx
+++ src/checkout/routes/CheckoutRoutes.tsx
@@ -2,14 +2,21 @@ import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { NotFound } from "../../components";
-import { Billing, Contact, Payment, Review, Shipping, ShippingOptions } from "../views";
+import {
+  Billing,
+  Contact,
+  Payment,
+  Review,
+  Shipping,
+  ShippingOptions,
+} from "../views";
 import { CheckoutRouteDispatcher } from "./CheckoutRouteDispatcher";
 
 import * as paths from ".";
 
 export const CheckoutRoutes: React.FC = () => (
   <Switch>
-    <Route exact path={paths.baseUrl} component={CheckoutRouteDispatcher} />
+    {/* <Route exact path={paths.baseUrl} component={CheckoutRouteDispatcher} /> */}
     <Route path={paths.contactUrl} component={Contact} />
     <Route path={paths.shippingAddressUrl} component={Shipping} />
     <Route path={paths.shippingOptionsUrl} component={ShippingOptions} />
