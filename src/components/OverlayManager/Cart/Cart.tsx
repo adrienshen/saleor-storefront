61973c4af4606dce7dd5c6e02032adae0edeb999
adrienshen <adrienshen.dev@gmail.com>
2020-07-13 23:33:44 -0700

    Calculate subtitle

--- src/components/OverlayManager/Cart/Cart.tsx
+++ src/components/OverlayManager/Cart/Cart.tsx
@@ -17,14 +17,12 @@ import { contactUrl } from "../../../checkout/routes";
 import { maybe } from "../../../core/utils";
 import { TypedProductVariantsQuery } from "../../../views/Product/queries";
 import { CartContext } from "../../CartProvider/context";
-import { extractCartLines, getTotal } from "../../CartProvider/utils";
+import { extractCartLines, getLineItemsTotal } from "../../CartProvider/utils";
 import { Error } from "../../Error";
 import Loader from "../../Loader";
 import { ShopContext } from "../../ShopProvider/context";
 import Empty from "./Empty";
 import ProductList from "./ProductList";
-
-import chevronUpImg from "../../../images/chevron-up.svg";
 import chevronLeft from "../../../images/chevron-left.svg";
 
 const Cart: React.FC<{ overlay: OverlayContextInterface }> = ({ overlay }) => {
@@ -86,7 +84,7 @@ export function CartBasic() {
                             <div className="cart__footer">
                               <div className="cart__footer__subtotoal">
                                 <div className="cart__footer__price-label">
-                                  <span>Total</span>
+                                  <span>Subtotal</span>
                                   <small>
                                     (
                                     {cart.lines
@@ -99,12 +97,8 @@ export function CartBasic() {
                                   </small>
                                 </div>
                                 <span className="cart__footer__price">
-                                  {getTotal(data, cart.lines, locale)}
+                                  {getLineItemsTotal(data, cart.lines, locale)}
                                 </span>
-                                <ReactSVG
-                                  path={chevronUpImg}
-                                  className="cart__footer__chevron-icon"
-                                />
                               </div>
                               <div className="cart__footer__button">
                                 <Link to={contactUrl}>
